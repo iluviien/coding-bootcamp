ae45c382f2545e219bc5f9d0531f4956d021b948
iluviien <stefanica695@gmail.com>
2023-03-31 02:59:24 +0000

    add

---
+++ Array-Challenge/src/index.js
@@ -0,0 +1,19 @@
+// Challenge 1
+// const fahrenheit = [110, 230, 98];
+// function getCelsius() {
+//   return fahrenheit.map(value => ((value - 32) * 5) / 9);
+// }
+// getCelsius();
+// console.log(getCelsius());
+// °C = (°F − 32) x 5/9
+// Challenge 2
+
+const array = [11, NaN, [], "Angels"];
+function checkForFalsey() {
+  return array.some(item => !item);
+}
+console.log(checkForFalsey());
+
+//Challenge 3
+const numberArrya = [9, 16, 81];
+function getTotal() {}
---
+++ balls/src/index.js
@@ -0,0 +1,23 @@
+//.length
+//Can be used on String or Array
+// const word='banana'
+//console.log('banana')
+//console.log(word.length)
+//const fruit= ['banana', 'apple', 'pear']
+//console.log(fruit.length)
+
+//
+
+//.concat
+
+const string = "Say Cheese, ";
+const string2 = "Alina";
+console.log(string.concat(string2));
+//arrays
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const array3 = [7, 8, 9];
+const newArray = array1.concat(array2, array3);
+console.log(newArray);
+console.log(array1.concat(array2));
